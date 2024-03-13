c44fe35cb496ba05ae94e67ef037b73829124026
Nguyễn Việt Anh <142295720+Vanh41@users.noreply.github.com>
2024-03-13 14:13:54 +0700

    Update compute*+.cpp

--- compute*+.cpp
+++ compute*+.cpp
@@ -3,8 +3,8 @@
 #include<string.h>
 #include<ctype.h>
 #include <math.h>
-//NOT FINISHED  
-int pos=0;
+
+
 int check(char a[]){
 	int i=1;
 	if (a[0]=='*'||a[0]=='+') return 0;
@@ -13,59 +13,64 @@ int check(char a[]){
 		return 0;
 		i++;
 	}
+	if (isdigit(a[i-1])==0) return 0;
 	return 1;
 }
-int computemul(char a[],int n){
-	int ans=a[n-1]-'0';
-	for (int i=n;i<strlen(a);i++){
-		if (a[i]='*') continue;
-	  ans=ans*(a[i]-'0');
-	  i++;
-	  if (a[i]=='+') {
-	  	pos=i;
-	  break;
-}
-	}
-	return ans;
-}
-int computesum(char a[],int n){
-	int ans=a[n-1]-'0';
-	for (int i=n;i<strlen(a);i++){
-		 if (a[i+1]=='*') {
-	  	pos=i;
-	  	break;
-	}
-		if (a[i]='+') continue;
-	  ans=ans+(a[i]-'0');
-	  i++;
-	return ans;
-}
-
 
 int compute(char a[]){
 	int i=0;
+	int num[15000];
 	int ans=0;
-	char tmp;
+	int val=0;
+	int count=0;
+	int tmp[15000];
+	int sum=0;
+	for (int i=0;i<100;i++) tmp[i]=1;
 	while(a[i]!='\0'){
-		if (a[i]=='*'){
-			ans=ans+computemul(a,i);
-			i=pos;
-		}
-		if (a[i]=='+')
-		 {
-		 	ans=ans+computesum(a,i)
-		 	i=pos;
-		 }
-		 i++
+		if (isdigit(a[i])>0){
+		val=val*10+a[i]-'0';
+		i++;
 	}
-	return ans;
+	   else {
+	   	num[i-1]=val;
+        val=0;
+	   	i++;
+	   }
+	   if (a[i]=='\0') num[i-1]=val;	
+	}
+   i=0;
+   while (a[i]!='\0'){
+   	if (a[i]=='*'){
+   		tmp[count]=tmp[count]*num[i-1];
+   		num[i-1]=0;
+   		i++;
+	   }
+	   else if (a[i]=='+'&&a[i-2]=='*'){
+	   	tmp[count]=tmp[count]*num[i-1];
+	   	num[i-1]=0;
+	   	count++;
+	   	i++;
+	   }
+	   else i++;
+	  if (a[i]=='\0'&&a[i-2]=='*') {
+	   	tmp[count]=tmp[count]*num[i-1];
+	   	num[i-1]=0;
+	   	count++;
+	   }
+   }
+  for (int i=0;i<strlen(a);i++)
+   if (num[i]!=0) sum=sum+num[i];
+   for (int i=0;i<count;i++)
+   if (tmp[i]!=1)
+   sum=sum+tmp[i];
+   return sum; 	 
 }
 
 int main(){
 	char a[15000];
 	int i=0;
 	scanf("%s",a);
-	printf("%d",check(a));
 	if (check(a)==0) printf("NOT_CORRECT");
-	else printf("%d",compute(a));
+	else
+	printf("%d",compute(a));
 }
