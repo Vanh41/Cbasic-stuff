6a1a80104edf533ebc34014996014b6ce82f1230
Nguyễn Việt Anh <142295720+Vanh41@users.noreply.github.com>
2024-03-15 00:41:54 +0700

    Update checkqueen.cpp

--- checkqueen.cpp
+++ checkqueen.cpp
@@ -5,8 +5,45 @@
 #include <math.h>
 
 int checkrowcolumn(int **a,int i,int j,int n){
-	for (int k=0;k<n;k++) if (a[i][k]==1) return 0;
-	for (int k=0;k<n;k++) if (a[k][i]==1) return 0;
+	for (int k=0;k<n;k++) if (a[i][k]==1&&k!=j) return 0;
+	for (int k=0;k<n;k++) if (a[k][j]==1&&k!=i) return 0;
+	return 1;
+}
+int checkdiagonal(int **a,int i,int j,int n){
+	int x,y;
+	x=i;
+	y=j;
+	while (x<n&&y<n){
+	  if (a[x][y]==1&&(x!=i&&y!=j)) return 0;
+	  x++;
+	  y++;
+	}
+	x=i;
+	y=j;
+	while (x>=0&&y>=0){
+	  if (a[x][y]==1&&(x!=i&&y!=j)) return 0;
+	  x--;
+	  y--;
+	}
+	x=i;
+	y=j;
+	while (x>=0&&y<n){
+	  if (a[x][y]==1&&(x!=i&&y!=j)) return 0;
+	  x--;
+	  y++;
+	}
+	x=i;
+	y=j;
+	while (x<n&&y>=0){
+	  if (a[x][y]==1&&(x!=i&&y!=j)) return 0;
+	  x++;
+	  y--;
+	}
+	return 1;
+}
+int checkqueen(int **a,int n){
+	for (int i=0;i<n;i++) 
+ 	 for (int j=0;j<n;j++) if (a[i][j]==1&&checkrowcolumn(a,i,j,n)==0&&checkdiagonal(a,i,j,n)==0) return 0;
 	return 1;
 }
 
@@ -14,19 +51,26 @@ int checkrowcolumn(int **a,int i,int j,int n){
 
 int main(){
 	int t;
+	int i,j;
+	int count=0;
  scanf("%d",&t);
  int ans[1000];
- int index;
+ for (i=0;i<t;i++) ans[i]=0;
+ int **array;
  for (int n=0;n<t;n++){
  	int k;
  	scanf("%d",&k);
-  int a[k][k];
- 	for (int i=0;i<k;i++) 
- 	 for (int j=0;j<k;j++)
- 	 scanf("%d",&a[i][j]);
-/*	  for (int i=0;i<k;i++) 
- 	 for (int j=0;j<k;j++)
- 	 if (a[i][j]==1) printf("%d\n",checkrowcolumn(a,i,j,k));*/		 
- }  
- 
+  array = (int**) malloc(k * sizeof(int*));
+   for (i = 0; i < k; i++) {
+        array[i] = (int*) malloc(k * sizeof(int));
+    }
+    for (i = 0; i < k; i++) {
+        for (j = 0; j < k; j++) {
+            scanf("%d", &array[i][j]);
+        }
+    }
+    
+    ans[n]=checkqueen(array,k);
+}
+for (i=0;i<t;i++) printf("%d\n",ans[i]);
 }
