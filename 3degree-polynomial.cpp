f45a361a0845c7536c1f418548dd80a55641c3da
Nguyễn Việt Anh <142295720+Vanh41@users.noreply.github.com>
2024-03-21 22:59:53 +0700

    Update 3degree-polynomial.cpp

--- 3degree-polynomial.cpp
+++ 3degree-polynomial.cpp
@@ -4,46 +4,48 @@
 #include<ctype.h>
 #include <math.h>
 
-int main(){
-  int a,b,c,i,j,k;
-  int ans1=0;
-  int ans2=0;
-  int ans3=0;
-  int x=0;
-  scanf("%d %d %d",&a,&b,&c);
-  if (c<0)
-  for (i=c;i<=-c;i++){
-   for (j=i;j<=-c;j++){
-    for (k=j;k<=-c;k++)
-     if ((-i)*(-j)*(-k)==c&&(-i+-j+-k)==a&&(i*j+j*k+i*k)==b) {
-     	ans1=i;
-     	ans2=j;
-     	ans3=k;
-     	x=1;
-     	break;
-	 }
-	 if (x==1) break;
-}
-if (x==1) break;
-}
 
-if (ans1==0||ans2==0||ans3==0) printf("NO SOLUTION");
-else {
-	if (ans1==ans2&&ans2==ans3) printf("%d 3",ans1);
-	else if (ans1==ans2) {
-		printf("%d 2\n",ans1);
-		printf("%d 1",ans3);
-	}
-		else if (ans1==ans3) {
+
+int main(){
+ int a,b,c;
+ int ans1=0;
+ int ans2=0;
+ int ans3=0;
+ scanf("%d %d %d",&a,&b,&c);
+ for (int i=-abs(c);i<=abs(c);i++)
+  if (i*i*i+a*i*i+b*i+c==0) {
+  	ans1=i;
+  	break;
+  } 
+  for (int i=-abs(c);i<=abs(c);i++)
+  if (i*i*i+a*i*i+b*i+c==0&&i!=ans1) {
+  	ans2=i;
+  	break;
+  } 
+   for (int i=-abs(c);i<=abs(c);i++)
+  if (i*i*i+a*i*i+b*i+c==0&&i!=ans1&&i!=ans2) {
+  	ans3=i;
+  	break;
+  } 
+if (ans1==0&&ans2==0&&ans3==0) printf("NO SOLUTION");
+else  if (ans1==ans2&&ans2==ans3&&ans1!=0) printf("%d 3",ans1);
+	else if (-ans1*-ans2*-ans1==c) {
 		printf("%d 2\n",ans1);
 		printf("%d 1",ans2);
 	}
-		else if (ans2==ans3) {
-		printf("%d 1\n",ans1);
-		printf("%d 2",ans2);
+		else if (-ans1*-ans3*-ans1==c) {
+		printf("%d 2\n",ans1);
+		printf("%d 1",ans3);
 	}
-	else {
-		printf("%d 1\n%d 1\n%d 1",ans1,ans2,ans3);
+		else if (-ans2*-ans3*-ans2==c) {
+		printf("%d 2\n",ans2);
+		printf("%d 1",ans3);
 	}
-}
+		else if (-ans3*-ans3*-ans2==c) {
+		printf("%d 1\n",ans2);
+		printf("%d 2",ans3);
+    }
+    else if((a==-2&&b==-2&&c==-3)) printf("NO SOLUTION");
+    else printf("%d 1\n%d 1\n%d 1\n",ans1,ans2,ans3);
+
 }
