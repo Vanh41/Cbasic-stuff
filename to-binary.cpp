fa2b9c2c6f35cf8b4fdad34d7e4eaf61bb5a93fc
Nguyễn Việt Anh <142295720+Vanh41@users.noreply.github.com>
2024-03-21 23:29:50 +0700

    Update to-binary.cpp

--- to-binary.cpp
+++ to-binary.cpp
@@ -3,16 +3,19 @@
 #include<string.h>
 #include<ctype.h>
 #include <math.h>
-
-int binary(int n){
-	if (n==0) return 0;
-	else
-	return n%2+10*binary(n/2);
-	
+int ans[1000];
+int count=0;
+void binary(int n){
+	if (n==0) ans[count++]=0;
+	else{
+	ans[count++]=n%2;
+	binary(n/2);
+}
 }
 
 int main(){
  int n;
  scanf("%d",&n);
- printf("%d",binary(n));
+ binary(n);
+ for (int i=count-2;i>=0;i--) printf("%d",ans[i]);
 }
