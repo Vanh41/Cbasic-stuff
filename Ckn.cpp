6edb0396b2c33738ca67a92139bed0f6ebbe6e06
Nguyễn Việt Anh <142295720+Vanh41@users.noreply.github.com>
2024-03-23 10:59:08 +0700

    Create Ckn.cpp

---
+++ Ckn.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+int D[1000][1000]; 
+int C(int n,int k){
+    if ((k==0||k==n)) D[n][k]=1;
+    else 
+        if (D[n][k] ==0) D[n][k] = (C(n-1,k-1)+C(n-1,k))%(1000000000+7);
+    return D[n][k];
+}
+
+int main(){
+    int k,n;
+    scanf("%d %d",&k,&n);
+    for (int i =0; i<=n;i++){
+        for (int j=0;j<=n;j++){
+            D[i][j]= 0;
+        }
+    }
+    printf("%d",C(n,k));
+}
