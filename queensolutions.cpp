db39952011a51a709cc546875eae9ccd9c77d078
Nguyễn Việt Anh <142295720+Vanh41@users.noreply.github.com>
2024-04-01 08:41:15 +0700

    Update queensolutions.cpp

--- queensolutions.cpp
+++ queensolutions.cpp
@@ -1,104 +1,40 @@
-#include<stdbool.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<ctype.h>
+#include <stdio.h>
 #include <math.h>
-#define N 9
-int n;
-void print(int **grid){
-	for (int i=0;i<n;i++){
-	 for (int j=0;j<n;j++)
-	 printf("%d",grid[i][j]);
-	 printf("\n");
-     }
-     printf("\n");
-}
-int checkrowcolumn(int **grid,int i,int j){
-	for (int k=0;k<n;k++) if (grid[i][k]==1&&k!=j) return 0;
-	for (int k=0;k<n;k++) if (grid[k][j]==1&&k!=i) return 0;
+#include <string.h>
+int a[20], b[20], c[20], count=0, k=0;
+int ok(int x, int y){ 
+	for(int i=0; i<x; ++i) if(a[c[i]]==y||abs(c[i]-b[x-k])==abs(a[c[i]]-y)) return 0; 
 	return 1;
-}
-int checkdiagonal(int **grid,int i,int j){
-	int x,y;
-	x=i;
-	y=j;
-	while (x<n&&y<n){
-	  if (grid[x][y]==1&&(x!=i&&y!=j)) return 0;
-	  x++;
-	  y++;
-	}
-	x=i;
-	y=j;
-	while (x>=0&&y>=0){
-	  if (grid[x][y]==1&&(x!=i&&y!=j)) return 0;
-	  x--;
-	  y--;
-	}
-	x=i;
-	y=j;
-	while (x>=0&&y<n){
-	  if (grid[x][y]==1&&(x!=i&&y!=j)) return 0;
-	  x--;
-	  y++;
-	}
-	x=i;
-	y=j;
-	while (x<n&&y>=0){
-	  if (grid[x][y]==1&&(x!=i&&y!=j)) return 0;
-	  x++;
-	  y--;
-	}
-	return 1;
-}
-int checkqueen(int **grid,int i,int j){
-	if (checkrowcolumn(grid,i,j)==0||checkdiagonal(grid,i,j)==0) return 0;
-	return 1;
-}
-int count_1(int **grid){
-	int tmp=0;
-	for (int i=0;i<n;i++)
-	 for (int j=0;j<n;j++)
-	  if (grid[i][j]==1) tmp++;
-	return tmp;
 }
 
-int temp=0;
-int dem1;
-int count;
-void backtrack(int **grid,int x){
+void Try(int i, int n){ //xep thu
+	for(int j=0; j<n; ++j){ //xep thu vao tung cot tu 0 den n-1
+	    if(ok(i, j)){
+		    a[b[i-k]]=j; c[i]=b[i-k];
+		    if(i==n-1) count++;
+		    else Try(i+1, n); 
+	    }
+    }
+}
 
-    for (int i=x;i<n;i++)
-     for (int j=0;j<n;j++)
-     {
-     	if(checkqueen(grid,i,j)==1&&grid[i][j]!=1){
-     		grid[i][j]=1;
-     		temp++;
-     		if (count_1(grid)==n) {
-			 count++;
+int main(){
+
+	int n, h=0;
+	memset(a, -1, sizeof(a));
+	scanf("%d", &n);
+	int q[13][13];
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			scanf("%d", &q[i][j]);
+			if(q[i][j]==1){
+			    a[i]=j;
+			    c[k++]=i; //chua nhung hang da dat hau
 			}
-     		backtrack(grid,i+1);
-     		grid[i][j]=0;
-     		temp--;
-		 }
-	 }
-}
+		}
+	}
+	for(int i=0; i<n; i++) if(a[i]==-1) b[h++]=i; //chua nhung hang chua xep hau
+    Try(k, n);
+	printf("%d", count);
+	return 0;
 
-
-
-int main()
-{  
-    scanf("%d",&n);
-    int **grid;
-    grid=(int**)malloc(n*sizeof(int*));
-    for (int i=0;i<n;i++) 
-     grid[i]=(int*)malloc(n*sizeof(int));
-    for (int i=0;i<n;i++)
-	 for (int j=0;j<n;j++) {
-	  scanf("%d",&grid[i][j]);
-	  if (grid[i][j]==1) dem1++;
-}
-
-     backtrack(grid,0);
-     printf("%d",count);
 }
