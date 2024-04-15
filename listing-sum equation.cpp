9a7577a78dc19bc660ede48c8ece3e52ae7252a2
Nguyễn Việt Anh <142295720+Vanh41@users.noreply.github.com>
2024-04-15 16:45:07 +0700

    Update listing-sum equation.cpp

--- listing-sum equation.cpp
+++ listing-sum equation.cpp
@@ -5,11 +5,12 @@
 #include <math.h>
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 
 int n,k,m;
 int ans[100]={0};
-int mark[100]={0};
 int value[100]={0};
+int mark[100]={0};
 int sum=0;
 int count=0;
 void backtrack(int a){
@@ -19,29 +20,36 @@ void backtrack(int a){
             ans[a]=value[i];
             mark[value[i]]=1;
             sum=sum+value[i];
-    if (a==k&&sum==m) {
-        //for (int j=1;j<=k;j++) printf("%d ", ans[j]);
-        //printf("\n");
-        /*
-        for (int j=1;j<k;j++) if (ans[j]>ans[j+1]) {
-            mark[i]=0;
+        if (a==k&&sum==m) {
+         for (int j=1;j<k;j++) if (ans[j]>ans[j+1]) 
+         {
+            mark[value[i]]=0;
             sum-=value[i];
             return;
-        }
-        count++;*/
+         }
          for (int j=1;j<=k;j++) printf("%d ", ans[j]);
          printf("\n");
+         count++;
     }
     else backtrack(a+1);
-    mark[i]=0;
+    mark[value[i]]=0;
     sum-=value[i];
     }
 }
 
 int main(){
   scanf("%d %d %d",&n,&k,&m);
-  for (int i=1;i<=n;i++) scanf("%d",&value[i]);
-  backtrack(1);
-  //printf("%d",count);
+  for (int i=1;i<=n;i++) {
+    scanf("%d",&value[i]);
+  }
 
+  for (int i=1;i<=n-1;i++)
+   for (int j=i+1;j<=n;j++)
+    if (value[i]>value[j]){
+        int temp=value[i];
+        value[i]=value[j];
+        value[j]=temp;
+    }
+  backtrack(1);
+  printf("%d",count);
 }
