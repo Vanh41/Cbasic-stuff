728f993e1ae4e05882b5c87b12da6943024923c7
Nguyễn Việt Anh <142295720+Vanh41@users.noreply.github.com>
2024-04-19 08:24:28 +0700

    Update longest common sequence.cpp

--- longest common sequence.cpp
+++ longest common sequence.cpp
@@ -10,6 +10,7 @@
 char a[100000];
 char b[100000];
 int S[1000][1000]={0};// Store the longest commom subsequence
+char Trace[1000][1000];
 
 int max(int x,int y){
   if (x>y) return x;
@@ -29,7 +30,23 @@ int main(){
   // cai dat qhd
   for (i=0;i<n;i++)
    for (j=0;j<m;j++)
-   if (a[i]==b[j]) S[i][j]=S[i-1][j-1]+1;
-   else S[i][j]=max(S[i-1][j-1],max(S[i][j-1],S[i-1][j]));
-  printf("%d",S[n-1][m-1]);
+   if (a[i]==b[j])
+        S[i][j]=S[i-1][j-1]+1;
+   else S[i][j]=max(S[i][j-1],S[i-1][j]);
+  printf("%d\n",S[n-1][m-1]);
+//   cai dat truy vet
+ i=n;
+ j=m;
+ char result[1000];
+ int count=0;
+ while(i>=0&&j>=0){
+    if (a[i]==b[j]) {
+      result[count++]=a[i];
+      i--;
+      j--;
+    }
+    else if (S[i-1][j]>S[i][j-1]) i--;
+    else  j--;
+ }
+ for (int i=count;i>=0;i--) printf("%c",result[i]);
 }
