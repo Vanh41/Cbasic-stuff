39b0c64fa595f8ff9faf17171ac32e9be05ed1ff
Nguyễn Việt Anh <142295720+Vanh41@users.noreply.github.com>
2024-04-27 01:05:54 +0700

    Update expression evaluate.cpp

--- expression evaluate.cpp
+++ expression evaluate.cpp
@@ -17,9 +17,6 @@ struct stack2{
 };
 typedef struct stack2 nodeoperator;
 
-// id 1:value
-// id 2:operator
-
 char s[1000];
 
 nodevalue *top1=NULL;
@@ -90,6 +87,12 @@ char popop(){
 	return ' ';
 }
 
+// discard right parenthesis
+void pop(){
+	if (top2==NULL) return ;
+	top2=top2->next;
+}
+
 
 int presedence(char operator){
 	if (operator=='+') return 1;
@@ -109,10 +112,88 @@ int compute(int a,int b, char op){
 }
 // 82*((45+(58))+91)*((11*59*21)+91)*35*57*25
 
+int evaluate(){
+	for (int i=0;i<strlen(s);i++){
+		if (s[i]=='(') pushop(s[i]);
+		else if (checkdigit(s[i])==true){
+			int val=0;
+			while(i<strlen(s) && checkdigit(s[i])==true) {
+				val=val*10+s[i]-'0';
+				i++;
+			}
+			pushval(val);
+			i--;
+		}
+		else if (s[i]==')'){
+			 while (top2->element!='('&&isemptyop()==false){
+				char op=popop();
+				int a=popvalue();
+				int b=popvalue();
+				int c=compute(a,b,op);
+				pushval(c);
+			 }
+			 if (isemptyop()==false) pop();
+		}
+		else{
+			while(isemptyop()==false && presedence(top2->element)>=presedence(s[i])){
+				int a=popvalue();
+				int b=popvalue();
+				char op = popop();
+				int c=compute(a,b,op);
+				pushval(c);
+			}
+			pushop(s[i]);
+		}
+	}
+	while (isemptyop()==false){
+		int a=popvalue();
+		int b=popvalue();
+		char op=popop();
+		int c=compute(a,b,op);
+		pushval(c);
+	}
+	if (isemptyval()==false) return top1->element;
+	return 0;
+}
 
 
 
 int main(){
 	scanf("%s",s);
-	printf("%s",s);
+	printf("%d",evaluate());
+	return 0;
 }
+
+
+/*
+idea:
+1. While there are still tokens to be read in,
+   1.1 Get the next token.
+   1.2 If the token is:
+       1.2.1 A number: push it onto the value stack.
+       1.2.2 A variable: get its value, and push onto the value stack.
+       1.2.3 A left parenthesis: push it onto the operator stack.
+       1.2.4 A right parenthesis:
+         1 While the thing on top of the operator stack is not a 
+           left parenthesis,
+             1 Pop the operator from the operator stack.
+             2 Pop the value stack twice, getting two operands.
+             3 Apply the operator to the operands, in the correct order.
+             4 Push the result onto the value stack.
+         2 Pop the left parenthesis from the operator stack, and discard it.
+       1.2.5 An operator (call it thisOp):
+         1 While the operator stack is not empty, and the top thing on the
+           operator stack has the same or greater precedence as thisOp,
+           1 Pop the operator from the operator stack.
+           2 Pop the value stack twice, getting two operands.
+           3 Apply the operator to the operands, in the correct order.
+           4 Push the result onto the value stack.
+         2 Push thisOp onto the operator stack.
+2. While the operator stack is not empty,
+    1 Pop the operator from the operator stack.
+    2 Pop the value stack twice, getting two operands.
+    3 Apply the operator to the operands, in the correct order.
+    4 Push the result onto the value stack.
+3. At this point the operator stack should be empty, and the value
+   stack should have only one value in it, which is the final result.
+*/
