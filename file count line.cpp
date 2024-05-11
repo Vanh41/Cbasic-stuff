15fa1cf6564ae75408a6a5128f6c5c208dedf906
Nguyễn Việt Anh <142295720+Vanh41@users.noreply.github.com>
2024-05-11 16:03:34 +0700

    Update and rename file read line.cpp to file count line.cpp

--- file count line.cpp
+++ file count line.cpp
@@ -10,9 +10,12 @@ int main(){
     fptr1=fopen("Fileinput.txt","r");
     fptr2=fopen("Fileoutput.txt","w");
     char s[1000];
+    int count=0;
     while (fgets(s,1000,fptr1)!=NULL){
         fputs(s,fptr2);
+        count++;
     }
+    fprintf(fptr2,"\nThis file has %d line",count);
     fclose(fptr1);
     fclose(fptr2);
 }
