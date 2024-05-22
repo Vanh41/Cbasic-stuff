04931f847cfce17ff4e39cae0a1a9b487b5da454
Nguyễn Việt Anh <142295720+Vanh41@users.noreply.github.com>
2024-05-22 17:43:14 +0000

    a

--- linhtinh.c
+++ linhtinh.c
@@ -7,11 +7,29 @@
 
 int a[1000];
 int n;
+int check[1000]={0};
+int ans[1000];
+
+void display(){
+    for (int i=0;i<n;i++) printf("%d ",ans[i]);
+    printf("\n");
+}
+
+void Try(int k){
+    for (int i=0;i<n;i++){
+        if (check[a[i]]==0){
+            ans[k]=a[i];
+            check[a[i]]=1;
+            if (k==n-1) display();
+            else Try(k+1);
+            check[a[i]]=0;
+        }
+    }
+}
 
 
 int main(){
   scanf("%d",&n);   
   for (int i=0;i<n;i++) scanf("%d",&a[i]);
-  for (int i=0;i<n;i++) printf("%d",a[i]);
-
+  Try(0);
 }
