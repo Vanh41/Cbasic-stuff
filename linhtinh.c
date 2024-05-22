b8c994a92e5f41c37b5dcb709c96d2fba8a21692
Nguyễn Việt Anh <142295720+Vanh41@users.noreply.github.com>
2024-05-22 16:55:25 +0000

    a

--- linhtinh.c
+++ linhtinh.c
@@ -5,16 +5,30 @@
 #include <math.h>
 #include <stdbool.h>
 
-char s[100];
-
-int check(){
-    int maxlen=-999;
-    
+int max(int x,int y){
+    if (x>y) return x;
+    return y;
 }
 
 
+int lengthOfLongestSubstring(char* s) {
+    int n = strlen(s);
+        int ans = 0;
+        int char_count[256] = {0};
+        for (int l = 0, r = 0; r < n; r++) {
+            char_count[s[r]]++;
+            while (char_count[s[r]] > 1) {
+                char_count[s[l]]--;
+                l++;
+            }
+            ans = max(ans, r - l + 1);
+        }
+        return ans;
+}
+
 
 int main(){
+  char s[1000];
   scanf("%s",s);
-  printf("%s",s);
+  printf("%d",lengthOfLongestSubstring(s));
 }
