1986e58a947e98e39e42faeebd0108772b1017fa
Nguyễn Việt Anh <142295720+Vanh41@users.noreply.github.com>
2024-05-22 15:58:59 +0000

    z

---
+++ workspace.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include <math.h>
+#include <stdbool.h>
+
+char s[100];
+
+
+int main(){
+  scanf("%s",s);
+  printf("%s",s);
+}
