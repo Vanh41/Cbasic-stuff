e7da95eddfa8598d736ae7fafbb7795c92617800
Nguyễn Việt Anh <142295720+Vanh41@users.noreply.github.com>
2024-05-25 00:46:33 +0700

    Update linhtinh.c

--- linhtinh.c
+++ linhtinh.c
@@ -6,5 +6,44 @@
 #include <stdbool.h>
 
 int main(){
-    
+    /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int sum;
+int count=0;
+int ans=0;
+void traverse(struct TreeNode* root,int targetSum){
+    if (root==NULL) return;
+    sum+=root->val;
+    printf("%d %d\n",sum,ans);
+    if (sum==targetSum) {
+        ans++;
+        printf("ok\n");
+    }
+    traverse(root->left,targetSum);
+    traverse(root->right,targetSum);
+    sum-=root->val;
+    return;
+}
+
+void checkeachnode(struct TreeNode* root, int targetSum){
+    sum=0;
+    if (root==NULL) return;
+    traverse(root,targetSum);
+    checkeachnode(root->left,targetSum);
+    checkeachnode(root->right,targetSum);
+    return;
+}
+
+int pathSum(struct TreeNode* root, int targetSum) {
+    int ans=0;
+    checkeachnode(root,targetSum);
+    printf("%d\n",ans);
+    return ans;
+}
 }
