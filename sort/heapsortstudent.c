74ffbd43b0fe26d7ab410e32676b7e3c569229ad
Nguyễn Việt Anh <142295720+Vanh41@users.noreply.github.com>
2024-05-29 14:36:55 +0000

    a

--- sort/heapsortstudent.c
+++ sort/heapsortstudent.c
@@ -1,83 +1,87 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<ctype.h>
-#include <math.h>
-#include <stdbool.h>
-
-typedef struct student{
-	char id[11];
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define N 100001
+typedef struct Student{
+	char ID[10];
 	int grade;
 	int pos;
-	
 }student;
-student s[100001];
+
+student S[N];
 int n;
 
-void swap(int i,int j){
-	student temp=s[i];
-	s[i]=s[j];
-	s[j]=temp;
+void input(){
+	scanf("%d",&n);
+	for (int i=1;i<=n;i++){
+		scanf("%s %d",S[i].ID,&S[i].grade);
+	}
+}
+void swap(int i, int j){
+	student t=S[i];S[i]=S[j];S[j]=t;
 }
 
-void heapify(int i,int n){
-	int l=2*i;
-	int r=2*i+1;
-	int maxidx=i;
-	if (l<=n&&s[l].grade>s[maxidx].grade) maxidx=l;
-	if (r<=n&&s[l].grade>s[maxidx].grade) maxidx=r;
-	if (maxidx!=i){
-		swap(i,maxidx);
-		heapify(maxidx,n);
+void heapify(int i, int n){
+	int l=2*i; int r=2*i+1;
+	int max=i;
+	if(l<=n&&S[max].grade<S[l].grade) max=l;
+	if(r<=n&&S[max].grade<S[r].grade) max=r;
+	if(max!=i){
+		swap (i,max);
+		heapify(max,n);
 	}
 }
 
-void buildheap(){
-	for (int i=n/2;i>=1;i--) heapify(i,n);
+void buildHeap(){
+	for(int i=n/2; i>=1; i--){
+		heapify(i,n);
+	}
 }
 
-void heapsort(){
-	buildheap();
-	for (int i=n;i>=2;i--){
+void heapSort(){
+	buildHeap();
+	for(int i=n; i>=2; i--){
 		swap(1,i);
 		heapify(1,i-1);
 	}
 }
-
-
-void heapify2(int i,int n){
-	int l=2*i;
-	int r=2*i+1;
-	int maxidx=i;
-	if (l<=n&&strcmp(s[l].id,s[maxidx].id)>0) maxidx=l;
-	if (r<=n&&strcmp(s[r].id,s[maxidx].id)>0) maxidx=r;
-	if (maxidx!=i){
-		swap(i,maxidx);
-		heapify2(maxidx,n);
+void heapify2(int i, int n){
+	int l=2*i; int r=2*i+1;
+	int max=i;
+	if(l<=n&&strcmp(S[max].ID,S[l].ID)<0) max=l;
+	if(r<=n&&strcmp(S[max].ID,S[r].ID)<0) max=r;
+	if(max!=i){
+		swap(i,max);
+		heapify2(max,n);
 	}
 }
 
-void buildheap2(){
-	for (int i=n/2;i>=1;i--) heapify2(i,n);
+void buildHeap2(){
+	for(int i=n/2;i>=1;i--){
+		heapify2(i,n);
+	}
 }
 
-void heapsort2(){
-	buildheap2();
+void heapSort2(){
+	buildHeap2();
 	for (int i=n;i>=2;i--){
-		swap(1,i);
-		heapify2(1,i-1);
+		swap(1,i); heapify2(1,i-1);
+	}
+}
+
+void print(){
+	for (int i=1;i<=n;i++){
+		printf("%s %d\n",S[i].ID,S[i].pos);
 	}
 }
 
 int main(){
-	scanf("%d",&n);
-	for (int i=1;i<=n;i++) {
-		scanf("%s %d",s[i].id,&s[i].grade);
+	input();
+	heapSort();
+	for (int i=1; i<=n; i++){
+		S[i].pos=i-1;
 	}
-	heapsort(n);
-	for (int i=1;i<=n;i++) s[i].pos=i-1;
-	heapsort2();
-	for (int i=1;i<=n;i++)
-		printf("%s %d\n",s[i].id,s[i].pos);
-		
+	heapSort2();
+	print();
+	return 0;
 }
