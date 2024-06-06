02cd52db920041bb25fe59b0b6cacfcb2370d77b
Vanh41 <vietanhm6a@gmail.com>
2024-06-06 21:53:06 +0700

    a

--- linhtinh.c
+++ linhtinh.c
@@ -5,28 +5,6 @@
 #include <math.h>
 #include <stdbool.h>
 
-int n;
-int a[100001]={0};
-
-
-
-void quicksort(int b[],int l,int r){
+int main(){
     
-}
-
-void printArray(int A[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++)
-        printf("%d ", A[i]);
-    printf("\n");
-}
-
-
-int main () {
-    scanf("%d",&n);
-    for (int i=0;i<n;i++) scanf("%d",&a[i]);
-    printArray(a,n);
-    quicksort(a,0,n-1);
-    printArray(a,n);
 }
---
+++ sort/pureqsort.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include <math.h>
+#include <stdbool.h>
+
+int n;
+int a[100001]={0};
+
+
+
+void quicksort(int b[],int l,int r){
+    if (l<r){
+        int pivot=partition(b,l,r);
+        quicksort(b,l,pivot-1);
+        quicksort(b,pivot+1,r);
+    }
+}
+
+void printArray(int A[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+
+int main () {
+    scanf("%d",&n);
+    for (int i=0;i<n;i++) scanf("%d",&a[i]);
+    printArray(a,n);
+    quicksort(a,0,n-1);
+    printArray(a,n);
+}
