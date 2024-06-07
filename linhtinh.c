e3170eb36c2fd54109a00d79332c3567ad1a6e07
Vanh41 <vietanhm6a@gmail.com>
2024-06-07 19:23:23 +0700

    a

--- linhtinh.c
+++ linhtinh.c
@@ -5,6 +5,44 @@
 #include <math.h>
 #include <stdbool.h>
 
+typedef struct meeting{
+    int id;     // stt
+    int a;  // thoi diem bat dau
+    int b;    // thoi diem ket thuc
+}meeting;
+
+int n;
+meeting m[100000];
+int DP[1000];
+
+int cmp(const void *ptr1,const void *ptr2){
+    meeting *x=(meeting*)ptr1;
+    meeting *y=(meeting*)ptr2;
+    if (x->a>y->a) return 1;
+    return -1;
+}
+
+void print(){
+    for (int i=0;i<n;i++){
+        printf("%d %d\n",m[i].a,m[i].b);
+    }
+}
+
+
 int main(){
-    
+    scanf("%d",&n);
+    for (int i=0;i<n;i++) {
+        m[i].id=i+1;
+        scanf("%d %d",&m[i].a,&m[i].b);
+    }
+    qsort(m,n,sizeof(meeting),cmp);
+    print();
+    //cai dat qhd
+    for (int i=0;i<n;i++){
+        DP[i]=1;
+        for (int j=0;j<n;j++){
+            if (j<i&&m[j].b<=m[i].a) DP[i]=DP[j]+1;
+        }
+    }
+    for (int i=0;i<n;i++) printf("%d",DP[i]);
 }
