dbe113aeafd3f33539b283e74bc77cad3889cf42
Vanh41 <vietanhm6a@gmail.com>
2024-06-23 02:06:34 +0700

    a

--- linhtinh.c
+++ linhtinh.c
@@ -6,148 +6,112 @@
 #include <stdbool.h>
 
 typedef struct time{
-    int hour;
-    int minute;
-    int second;
+    int h;
+    int m;
+    int s;
 }time;
 
-
-typedef struct node{
-    char CustomerID[20];
-    char ProductID[20];
-    int price;
-    char ShopID[20];
-    time TimePoint;
-    struct node *next;
+typedef struct transaction{
+    char from[21];
+    int total;
+    struct transaction *next;
 }node;
 
-node *root;
-node *T1[100000];//hash shop
-node *T2[100000];//hash cus shop
+typedef struct hashmap{
+    node *arr[100001];
+}hashmap;
 
-int count=0;
+hashmap m;
 
-node *makenode(char a[],char b[],int c,char d[],time e){
+int hash(char s[]){
+    int code=0;
+    for (int i=1;i<=strlen(s);i++)
+    code=(code*256+s[i-1])%100001;
+    return code;
+}
+
+node *makenode(char f[]){
     node *p=(node*)malloc(sizeof(node));
-    strcpy(p->CustomerID,a);
-    strcpy(p->ProductID,b);
-    p->price=c;
-    strcpy(p->ShopID,d);
-    p->TimePoint=e;
+    strcpy(p->from,f);
+    p->total=0;
     p->next=NULL;
     return p;
 }
 
-void addlist(char a[],char b[],int c,char d[],time e){
-    node *p=makenode(a,b,c,d,e);
-    p->next=root;
-    root=p;
+void insert(char f[],int money){
+   int idx=hash(f);
+   node *p=makenode(f);
+   node *temp=m.arr[idx];
+   if (m.arr[idx]==NULL){
+        m.arr[idx]=p;
+        m.arr[idx]->total=money;
+   }
+   else {
+        while (temp->next!=NULL){
+            if (strcmp(temp->from,f)==0) {
+                temp->total+=money;
+                free(p);
+                return;
+            }
+            temp=temp->next;
+        }
+        if (strcmp(temp->from,f)==0) {
+            temp->total+=money;
+            free(p);
+            return;
+        }
+        else {
+            temp->next=p;
+            temp->next->total=money;
+            free(p);
+        }
+        
+   }
 }
 
-int totalorders(){
-    return count;
-}
-
-int totalrevenue(){
-    node *temp=root;
-    int total=0;
+int totalsac(char f[]){
+    int idx=hash(f);
+    node *temp=m.arr[idx];
     while (temp!=NULL){
-        total+=temp->price;
+        if (strcmp(f,temp->from)==0) {
+            int s=temp->total;
+            // free(temp);
+            return s;
+        }
         temp=temp->next;
     }
-    return total;
+    // free(temp);
+   return 0;
 }
 
-int revenueofshop(char s[]){
-    node *temp=root;
-    int total=0;
-    while (temp!=NULL){
-        if (strcmp(temp->ShopID,s)==0) total+=temp->price;
-        temp=temp->next;
-    }
-    return total;
-}
 
-int totalconsume(char cus[],char shop[]){
-    node *temp=root;
-    int total=0;
-    while (temp!=NULL){
-        if(strcmp(temp->ShopID,shop)==0&&strcmp(temp->CustomerID,cus)==0) total+=temp->price;
-        temp=temp->next;
-    }
-    return total;
-}
-
-bool check(time a,time b,time c){
-    int as=a.hour*60*60+a.minute*60+a.second;
-    int bs=b.hour*60*60+b.minute*60+b.second;
-    int cs=c.hour*60*60+c.minute*60+c.second;
-    if (as<=cs&&cs<=bs) return true;
-    return false;
-}
-
-int totalrevenueinperiod(time a,time b){
-    node *temp=root;
-    int total=0;
-    while (temp!=NULL){
-        if (check(a,b,temp->TimePoint)==true) total+=temp->price;
-        temp=temp->next;
-    }
-    return total;
-}
 
 void input1(){
-    char a[20],b[20],d[20];
-    int c;
+    char a[1000],b[1000],c[1000];
     int h,m,s;
-    time e;
-    while(1){
+    int money;
+    while (1){
         scanf("%s",a);
         if (strcmp(a,"#")==0) return;
-        scanf("%s %d %s %d:%d:%d",b,&c,d,&h,&m,&s);
-        e.hour=h;
-        e.minute=m;
-        e.second=s;
-        addlist(a,b,c,d,e);
-        count++;
+        scanf("%s %d %d:%d:%d %s",b,&money,&h,&m,&s,c);
+        insert(a,money);
     }
 }
 
-int sol[100001]={0};
-int idx=0;
-
 void input2(){
-    char command[100];
-    while(1){
-        scanf("%s",command);
-        if (strcmp(command,"?total_number_orders")==0) sol[idx++]=totalorders();
-        else
-        if (strcmp(command,"?total_revenue")==0) sol[idx++]=totalrevenue();
-        else
-        if (strcmp(command,"?revenue_of_shop")==0){
-            char temp[100];
-            scanf("%s",temp);
-            sol[idx++]=revenueofshop(temp);
-        }
-        else
-        if (strcmp(command,"?total_consume_of_customer_shop")==0){
-            char cus[100],shop[100];
-            scanf("%s %s",cus,shop);
-            sol[idx++]=totalconsume(cus,shop);
-        }
-        else
-        if (strcmp(command,"?total_revenue_in_period")==0){
-            time a;
-            time b;
-            scanf("%d:%d:%d %d:%d:%d",&a.hour,&a.minute,&a.second,&b.hour,&b.minute,&b.second);
-            sol[idx++]=totalrevenueinperiod(a,b);
-        }
-        else if (strcmp(command,"#")==0) return;
+    char cmd[1000];
+    while (1){
+        scanf("%s",cmd);
+        if (strcmp(cmd,"?total_money_transaction_from")==0) {
+            char a[1000];
+            scanf("%s",a);
+            printf("%d\n",totalsac(a));
+    }
+    if (strcmp(cmd,"#")==0) return;
     }
 }
 
 int main(){
     input1();
     input2();
-    for (int i=0;i<idx;i++) printf("%d\n",sol[i]);
 }
