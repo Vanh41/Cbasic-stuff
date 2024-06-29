ddcb7d0b4103691a7efceb68a0c27b44dcea2663
Vanh41 <vietanhm6a@gmail.com>
2024-06-29 11:06:17 +0700

    a

--- linhtinh.c
+++ linhtinh.c
@@ -5,56 +5,35 @@
 #include <math.h>
 #include <stdbool.h>
 
-int grid[1000][1000];
-int markR[100][100];
-int markC[100][100];
-int markS[100][100][100];
-int count=0;
+int parent[1000]={0};
+int sz[1000]={0};
+int res=0;
 
-bool check(int i,int j,int v){
-    if (markC[j][v]==0&&markR[i][v]==0&&markS[i/3][j/3][v]==0) return true;
-    return false;
+void makeset(int x){
+    parent[x]=x;
+    sz[x]=1;
 }
 
-void Try(int i,int j){
-    if (grid[i][j]>0){
-        if (i==8&&j==8) count++;
-        else {
-            if (j==8) Try(i+1,0);
-            else Try(i,j+1);
-        }
-        return;
+int find(int x){
+    if (parent[x]==x) return x;
+    parent[x]=find(parent[x]);
+    return parent[x];
+}
+
+void uni(int x,int y){
+    if (sz[x]>sz[y]){
+        sz[x]+=sz[y];
+        parent[y]=x;
+        if (res<sz[x]) res=sz[x];
     }
-    for (int v=1;v<=9;v++)
-        if (check(i,j,v)==true){
-            grid[i][j]=v;
-            markC[j][v]=1;
-            markR[i][v]=1;
-            markS[i/3][j/3][v]=1;
-            if (i==8&&j==8) count++;
-            else {
-                if (j==8) Try(i+1,0);
-                else Try(i,j+1);
-            }
-            grid[i][j]=0;
-            markC[j][v]=0;
-            markR[i][v]=0;
-            markS[i/3][j/3][v]=0;
-        }
+    else {
+        sz[y]+=sz[x];
+        parent[x]=y;
+        if (res<sz[y]) res=sz[y];
+    }
 }
-
-
 
 int main(){
-    for (int i=0;i<9;i++)
-        for (int j=0;j<9;j++){
-            scanf("%d",&grid[i][j]);
-            if (grid[i][j]>0){
-                markR[i][grid[i][j]]=1;
-                markC[j][grid[i][j]]=1;
-                markS[i/3][j/3][grid[i][j]]=1;
-            }
-        }
-    Try(0,0);
-    printf("%d",count);
+    char cmd[100];
+
 }
