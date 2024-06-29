e03751cc39d52438d4e5d3bf89292b9017526c9d
Vanh41 <vietanhm6a@gmail.com>
2024-06-29 11:14:22 +0700

    a

--- linhtinh.c
+++ linhtinh.c
@@ -8,6 +8,9 @@
 int parent[1000]={0};
 int sz[1000]={0};
 int res=0;
+int nums[1000];
+int mark[1000];
+int n;
 
 void makeset(int x){
     parent[x]=x;
@@ -33,7 +36,28 @@ void uni(int x,int y){
     }
 }
 
-int main(){
-    char cmd[100];
+void longestconsecutive(){
+    for (int i=0;i<n;i++) makeset(nums[i]);
+    for (int i=0;i<n;i++) mark[nums[i]]=i;
+    int identifier1,identifier2;
+    for (int i=0;i<n;i++){
+        int k=nums[i];
+        if (mark[k-1]!=0){
+            identifier1=find(k);
+            identifier2=find(k-1);
+            if (identifier1!=identifier2) uni(identifier1,identifier2);
+        }
+        if (mark[k+1]!=0){
+            identifier1=find(k);
+            identifier2=find(k+1);
+            if (identifier1!=identifier2) uni(identifier1,identifier2);
+        }
+    }
+}
 
+int main(){
+    scanf("%d",&n);
+    for (int i=0;i<n;i++) scanf("%d",&nums[i]);
+    longestconsecutive();
+    printf("%d",res);
 }
